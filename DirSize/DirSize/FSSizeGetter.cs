50e601ae3209a44c59e2f9ec7ce2a783a7d5bd0a
hayatoyu <f19870421@gmail.com>
2018-05-15 21:05:25 +0800

    v1.0

---
+++ DirSize/DirSize/FSSizeGetter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DirSize
+{
+    class FSSizeGetter
+    {
+        enum SizeUnit
+        {
+            Bytes,KB,MB,GB,TB,PB,EB,ZB,YB
+        }
+
+        public long getDirSize(string path)
+        {
+            long size = 0;
+
+            DirectoryInfo dinfo = new DirectoryInfo(path);
+
+            // 計算資料夾中的檔案總大小
+            var files = dinfo.GetFiles();
+            foreach (var f in files)
+                size += f.Length;
+
+            // 遞迴計算子資料夾中的檔案大小
+            var childrenDir = dinfo.GetDirectories();
+            foreach (var dir in childrenDir)
+                size += getDirSize(dir.FullName);
+
+            return size;
+        }
+
+        public long getDirSizeByLinq(string path)
+        {
+            return (from file in Directory.GetFiles(path, "*.*", SearchOption.AllDirectories) select new FileInfo(file).Length).Sum();
+        }
+
+        public long getDirSizeByScripting(string path)
+        {
+            return long.Parse((new Scripting.FileSystemObjectClass()).GetFolder(path).Size.ToString());
+        }
+
+        public long getDirSizeByReflection(string path)
+        {
+            Type tp = Type.GetTypeFromProgID("Scripting.FileSystemObject");
+            var fso = Activator.CreateInstance(tp);
+            var folder = tp.InvokeMember("GetFolder", System.Reflection.BindingFlags.InvokeMethod, null, fso, new object[] { path });
+            long size = Convert.ToInt64(tp.InvokeMember("Size", System.Reflection.BindingFlags.GetProperty, null, folder, null));
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(fso);
+            return size;
+        }
+
+        public string formatSize(long size)
+        {
+            double formatSize = 0;
+            int count = 0;
+
+            formatSize = size;
+            while(formatSize > 1024 && count < 9)
+            {
+                formatSize /= 1024;
+                count++;
+            }
+            return Math.Round(formatSize, 2).ToString() + " " + (SizeUnit)count;
+        }
+    }
+}
---
+++ DirSize/DirSize/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace DirSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FSSizeGetter getter = new FSSizeGetter();
+
+            var watch = Stopwatch.StartNew();
+            Console.WriteLine("DirSize : " + getter.formatSize(getter.getDirSize(@"D:\01lin")));
+            Console.WriteLine(watch.ElapsedMilliseconds + " ms spent.\n");
+
+            watch.Restart();
+            Console.WriteLine("DirSize : " + getter.formatSize(getter.getDirSizeByLinq(@"D:\01lin")));
+            Console.WriteLine(watch.ElapsedMilliseconds + " ms spent.\n");
+
+            watch.Restart();
+            Console.WriteLine("DirSize : " + getter.formatSize(getter.getDirSizeByScripting(@"D:\01lin")));
+            Console.WriteLine(watch.ElapsedMilliseconds + " ms spent.\n");
+
+            watch.Restart();
+            Console.WriteLine("DirSize : " + getter.formatSize(getter.getDirSizeByReflection(@"D:\01lin")));
+            Console.WriteLine(watch.ElapsedMilliseconds + " ms spent.\n");
+
+            watch.Stop();
+        }
+    }
+}
